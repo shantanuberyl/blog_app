865d22383d86a808175b9a0b4dc87ed6de096ab8
Shantanu <shantanu.rana@berylsystems.com>
2018-01-23 19:10:48 +0530

    create new template, action, route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,19 @@ class ArticlesController < ApplicationController
     #code
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "articles#home"
+  get 'new', to: 'articles#new'
+
+  resources :articles
 end
