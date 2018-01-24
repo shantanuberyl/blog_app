bb27634872a2a0e3eb66c7e9bfe31e47e4e95e64
Shantanu <shantanu.rana@berylsystems.com>
2018-01-24 14:51:27 +0530

    added flash and error msg, create show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "articles#home"
-  get 'new', to: 'articles#new'
-
+  root 'articles#home'
+  get 'article/home', to: 'articles#home'
+  get 'article/new', to: 'articles#new'
+  
   resources :articles
 end
