11109f1be447b989b9902da875fc3f973a8d90b9
Shantanu <shantanu.rana@berylsystems.com>
2018-01-30 11:51:54 +0530

    Accociations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 5, maximum: 30 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 80 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 200 }
+  validates :user_id, presence: true
 end
